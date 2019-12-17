bec1929f438be708cdf274496867470e1cb60769
Rob Ramsay <robert.ramsay@slalom.com>
2019-12-17 15:20:27 -0800

    Some refactoring before Day 3 Part 2

--- AoC 2019/WireManagement/Intersection.cs
+++ AoC 2019/WireManagement/Intersection.cs
@@ -14,6 +14,13 @@ namespace WireManagement
         /// </summary>
         /// <param name="x">X coordinate</param>
         /// <param name="y">Y coordinate</param>
+        public Intersection(int x, int y) : base(x, y) { }
+
+        /// <summary>
+        /// Initializes a new Intersection object
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
         /// <param name="lengthA">Length of first path leading to this intersection.</param>
         /// <param name="lengthB">Length of second path leading to this intersection.</param>
         public Intersection(int x, int y, int lengthA, int lengthB) : base(x, y)
--- AoC 2019/WireManagement/IntersectionDistanceCalculator.cs
+++ AoC 2019/WireManagement/IntersectionDistanceCalculator.cs
@@ -16,9 +16,9 @@ namespace WireManagement
         private int Ymax = 0;
         private int Ymin = 0;
 
-        private List<KeyValuePair<int, int>> intersectionList = new List<KeyValuePair<int, int>>();
+        private List<Intersection> intersectionList = new List<Intersection>();
 
-        public int FindSmallestManhattanDistance(KeyValuePair<char, int>[] wire1, KeyValuePair<char, int>[] wire2)
+        public int FindSmallestManhattanDistance(Command[] wire1, Command[] wire2)
         {
             this.CalculateBoundaries(wire1);
             this.CalculateBoundaries(wire2);
@@ -35,7 +35,6 @@ namespace WireManagement
             this.WireGrid = new int[this.maxXsize, this.maxYsize];
             this.PlotTrack(wire1, wire1ID, startX, startY);
             this.PlotTrack(wire2, wire2ID, startX, startY);
-            //this.FindIntersections(startX, startY, maxXsize, maxYsize, wire1ID + wire2ID);
 
             int minValue = int.MaxValue;
             for(int i = 0; i < intersectionList.Count; i++)
@@ -50,22 +49,22 @@ namespace WireManagement
             return minValue;
         }
 
-        public int FindManhattanDistance(KeyValuePair<int,int> coordinate, int originX, int originY)
+        public int FindManhattanDistance(Intersection coordinate, int originX, int originY)
         {
-            return Math.Abs((Math.Abs(coordinate.Key) - Math.Abs(originX))) + Math.Abs((Math.Abs(coordinate.Value) - Math.Abs(originY)));
+            return Math.Abs((Math.Abs(coordinate.X) - Math.Abs(originX))) + Math.Abs((Math.Abs(coordinate.Y) - Math.Abs(originY)));
         }
 
-        private void CalculateBoundaries(KeyValuePair<char,int>[] commands)
+        private void CalculateBoundaries(Command[] commands)
         {
             int curX = 0;
             int curY = 0;
 
             for(int i = 0; i < commands.Length; i++)
             {
-                switch (commands[i].Key)
+                switch (commands[i].Direction)
                 {
-                    case 'U':
-                        curY += commands[i].Value;
+                    case Direction.Up:
+                        curY += commands[i].Distance;
 
                         if (curY > this.Ymax)
                         {
@@ -76,8 +75,8 @@ namespace WireManagement
                             this.Ymin = curY;
                         }
                         break;
-                    case 'D':
-                        curY -= commands[i].Value;
+                    case Direction.Down:
+                        curY -= commands[i].Distance;
 
                         if(curY > this.Ymax)
                         {
@@ -88,8 +87,8 @@ namespace WireManagement
                             this.Ymin = curY;
                         }
                         break;
-                    case 'L':
-                        curX -= commands[i].Value;
+                    case Direction.Left:
+                        curX -= commands[i].Distance;
 
                         if (curX > this.Xmax)
                         {
@@ -100,8 +99,8 @@ namespace WireManagement
                             this.Xmin = curX;
                         }
                         break;
-                    case 'R':
-                        curX += commands[i].Value;
+                    case Direction.Right:
+                        curX += commands[i].Distance;
 
                         if(curX > this.Xmax)
                         {
@@ -116,26 +115,26 @@ namespace WireManagement
             }
         }
 
-        private void PlotTrack(KeyValuePair<char, int>[] commands, int trackNum, int startX, int startY)
+        private void PlotTrack(Command[] commands, int trackNum, int startX, int startY)
         {
             int curX = startX;
             int curY = startY;
 
             for (int i = 0; i < commands.Length; i++)
             {
-                switch (commands[i].Key)
+                switch (commands[i].Direction)
                 {
-                    case 'U':
-                        this.TraverseInstruction(ref curX, ref curY, commands[i].Value, trackNum, 1, ref curY);
+                    case Direction.Up:
+                        this.TraverseInstruction(ref curX, ref curY, commands[i].Distance, trackNum, 1, ref curY);
                         break;
-                    case 'D':
-                        this.TraverseInstruction(ref curX, ref curY, commands[i].Value, trackNum, -1, ref curY);
+                    case Direction.Down:
+                        this.TraverseInstruction(ref curX, ref curY, commands[i].Distance, trackNum, -1, ref curY);
                         break;
-                    case 'L':
-                        this.TraverseInstruction(ref curX, ref curY, commands[i].Value, trackNum, -1, ref curX);
+                    case Direction.Left:
+                        this.TraverseInstruction(ref curX, ref curY, commands[i].Distance, trackNum, -1, ref curX);
                         break;
-                    case 'R':
-                        this.TraverseInstruction(ref curX, ref curY, commands[i].Value, trackNum, 1, ref curX);
+                    case Direction.Right:
+                        this.TraverseInstruction(ref curX, ref curY, commands[i].Distance, trackNum, 1, ref curX);
                         break;
                 }
             }
@@ -152,7 +151,7 @@ namespace WireManagement
                 else if (this.WireGrid[curX, curY] != trackNum)
                 {
                     this.WireGrid[curX, curY] += trackNum;
-                    this.intersectionList.Add(new KeyValuePair<int, int>(curX, curY));
+                    this.intersectionList.Add(new Intersection(curX, curY));
                 }
                 axisValue += delta;
             }
@@ -161,12 +160,10 @@ namespace WireManagement
 
     public static class ExtensionMethods
     {
-        public static KeyValuePair<char,int>[] ConvertToCommands(this string input)
+        public static Command[] ConvertToCommands(this string input)
         {
-            List<KeyValuePair<char, int>> output = new List<KeyValuePair<char, int>>();
+            List<Command> output = new List<Command>();
             string[] inputArr = input.Split(',');
-
-            Regex alpha = new Regex("[^a-zA-Z]");
 
             for(int i = 0; i < inputArr.Length; i++)
             {
@@ -174,12 +171,21 @@ namespace WireManagement
                 {
                     throw new ArgumentException($"ERROR: Step {inputArr[i]} is not valid!");
                 }
-                else if(!Regex.IsMatch(inputArr[i], "[a-zA-Z]{1}[0-9]+"))
+                else if(!Regex.IsMatch(inputArr[i], "[u|d|l|r|U|D|L|R]{1}[0-9]+"))
                 {
                     throw new ArgumentException($"ERROR: Step {inputArr[i]} is not valid!");
                 }
 
-                output.Add(new KeyValuePair<char, int>(inputArr[i][0], Convert.ToInt32(inputArr[i].Substring(1))));
+                Direction direction;
+                switch (inputArr[i][0])
+                {
+                    case 'U': direction = Direction.Up; break;
+                    case 'D': direction = Direction.Down; break;
+                    case 'L': direction = Direction.Left; break;
+                    case 'R': direction = Direction.Right; break;
+                    default: direction = Direction.Up; break; // it will never get here because of the regex above, but the compiler complains without it.
+                }
+                output.Add(new Command(direction, Convert.ToInt32(inputArr[i].Substring(1))));
             }
 
             return output.ToArray();
